3e84352d081bc400adc96a4ff59ad0c4f21c1e51
Arun Milton <miltononline@gmail.com>
2019-03-17 13:20:47 -0400

    Introduction-to-Express-Assignment-Completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,23 +1,38 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const baseMacro = "http://localhost:3000/macro";
 
 describe("routes : static", () => {
 
-//#1
   describe("GET /", () => {
 
-//#2
     it("should return status code 200", (done) => {
 
-//#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
 
-//#4
+        done();
+      });
+    });
+  });
+  describe("GET /macro", () => {
+
+    it("should return status code 200", (done) => {
+
+      request.get(baseMacro, (err, res, body) => {
+        expect(res.statusCode).toBe("polo");
+
         done();
       });
     });
 
+    it("should return string 'Polo' ", (done) => {
+      request.get(baseMacro, (err, res, body) => {
+        expect(body).toContain('Polo');
+
+        done();
+      });
+    });
   });
 });
