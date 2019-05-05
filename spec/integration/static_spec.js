eeb73102c03b317ea492d70edcaee62255c882f1
Arun Milton <miltononline@gmail.com>
2019-05-05 13:44:02 -0400

    assingment-completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,11 +6,23 @@ describe("routes : static", () => {
 
   describe("GET /", () => {
 
-  it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
 
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
+
+        done();
+      });
+    });
+  });
+  describe("GET /about", () => {
+
+    it("should return status code 200 and have 'About us' in the body of the response", (done) => {
+
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us");
 
         done();
       });
