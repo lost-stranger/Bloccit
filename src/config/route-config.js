65add7cc21f0f16e15fdceb9ef09b92986f9272c
Arun Milton <miltononline@gmail.com>
2019-05-26 07:29:30 -0400

    checkpoint-progress

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -6,6 +6,8 @@ module.exports = {
     const flairRoutes = require("../routes/flairs");
     const userRoutes = require("../routes/users");
     const commentRoutes = require("../routes/comments");
+    const voteRoutes = require("../routes/votes");
+
 
     if(process.env.NODE_ENV === "test") {
       const mockAuth = require("../../spec/support/mock-auth.js");
@@ -17,6 +19,8 @@ module.exports = {
     app.use(flairRoutes);
     app.use(userRoutes);
     app.use(commentRoutes);
+    app.use(voteRoutes);
+
 
   }
 }
