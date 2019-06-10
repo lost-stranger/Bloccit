a3a8e1b2e5282374891d3503c055368c011dfb80
Arun Milton <miltononline@gmail.com>
2019-06-10 06:02:15 -0400

    checkpoint-halfway-point

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -13,48 +13,53 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false
     },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  }
-}, {});
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {});
 
-Post.associate = function(models) {
+  Post.associate = function(models) {
 
-  Post.belongsTo(models.Topic, {
-    foreignKey: "topicId",
-    onDelete: "CASCADE"
-  });
-  Post.belongsTo(models.User, {
-    foreignKey: "userId",
-    onDelete: "CASCADE"
-  });
+    Post.belongsTo(models.Topic, {
+      foreignKey: "topicId",
+      onDelete: "CASCADE"
+    });
+    Post.belongsTo(models.User, {
+      foreignKey: "userId",
+      onDelete: "CASCADE"
+    });
 
-  Post.hasMany(models.Flair, {
-    foreignKey: "postId",
-    as: "flairs"
-  });
+    Post.hasMany(models.Flair, {
+      foreignKey: "postId",
+      as: "flairs"
+    });
 
-  Post.hasMany(models.Comment, {
-     foreignKey: "postId",
-     as: "comments"
-   });
+    Post.hasMany(models.Comment, {
+      foreignKey: "postId",
+      as: "comments"
+    });
+    
+    Post.hasMany(models.Favorite, {
+      foreignKey: "postId",
+      as: "favorites"
+    });
 
-   Post.hasMany(models.Vote, {
-  foreignKey: "postId",
-  as: "votes"
-});
+    Post.hasMany(models.Vote, {
+      foreignKey: "postId",
+      as: "votes"
+    });
 
-};
-Post.prototype.getPoints = function(){
+  };
+  Post.prototype.getPoints = function(){
 
-// #1
-  if(this.votes.length === 0) return 0
+    // #1
+    if(this.votes.length === 0) return 0
 
-// #2
-  return this.votes
+    // #2
+    return this.votes
     .map((v) => { return v.value })
     .reduce((prev, next) => { return prev + next });
-};
-return Post;
+  };
+  return Post;
 };
--- src/db/models/user.js
+++ src/db/models/user.js
@@ -24,13 +24,17 @@ module.exports = (sequelize, DataTypes) => {
       as: "posts"
     });
     User.hasMany(models.Comment, {
-  foreignKey: "userId",
-  as: "comments"
-});
-User.hasMany(models.Vote, {
-  foreignKey: "userId",
-  as: "votes"
-});
+      foreignKey: "userId",
+      as: "comments"
+    });
+    User.hasMany(models.Favorite, {
+      foreignKey: "userId",
+      as: "favorites"
+    });
+    User.hasMany(models.Vote, {
+      foreignKey: "userId",
+      as: "votes"
+    });
   };
   User.prototype.isAdmin = function() {
     return this.role === "admin";
